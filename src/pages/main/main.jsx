013310b4d7a5b8a27b714e0c3b1f191a49967ce9
gongjuyeonhee <yyeonhee912@naver.com>
2024-09-15 16:59:49 +0900

    feat: Adjust CSS for textarea and button to improve layout and focus styles

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./main.scss";
 
 export const Main = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const messageListRef = useRef(null); // 메시지 목록 참조
+  const textareaRef = useRef(null);
 
   const handleSendMessage = () => {
     if (message.trim() !== "") {
@@ -15,8 +17,16 @@ export const Main = () => {
         }),
       };
 
-      setMessages([newMessage, ...messages]);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
       setMessage("");
+
+      // 전송 후 약간 지연 후 메시지 목록이 보이도록 스크롤
+      setTimeout(() => {
+        if (messageListRef.current) {
+          messageListRef.current.scrollTop =
+            messageListRef.current.scrollHeight;
+        }
+      }, 0); // 0ms 지연
     }
   };
 
@@ -27,26 +37,36 @@ export const Main = () => {
     }
   };
 
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [message]);
+
   return (
     <div className="main-container">
+      <div className="message-list" ref={messageListRef}>
+        {messages.map((msg, index) => (
+          <div key={index} className="message-item">
+            <div className="message">
+              <p>{msg.text}</p>
+            </div>
+            <span>{msg.time}</span>
+          </div>
+        ))}
+      </div>
       <div className="message-input">
-        <input
-          type="text"
+        <textarea
+          ref={textareaRef}
           placeholder="메시지를 입력하세요"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           onKeyDown={handleKeyDown}
+          rows={1}
+          style={{ resize: "none", overflow: "hidden" }}
         />
         <button onClick={handleSendMessage}>전송</button>
-      </div>
-
-      <div className="message-list">
-        {messages.map((msg, index) => (
-          <div key={index} className="message-item">
-            <p>{msg.text}</p>
-            <span>{msg.time}</span>
-          </div>
-        ))}
       </div>
     </div>
   );
